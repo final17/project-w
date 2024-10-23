5e4da9343228bd65c759904446aee95a419fa1bc
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-23 10:41:43 +0900

    [update] 필요없는 도메인 삭제

--- src/main/java/com/projectw/domain/confirmation/repository/ConfirmationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projectw.domain.confirmation.repository;
-
-import com.projectw.domain.confirmation.entity.Confirmation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfirmationRepository extends JpaRepository<Confirmation, Long> , ConfirmationDslRepository {
-}
