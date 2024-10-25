a054c62ace1a37492530cea5bb34dc913bac3e37
yunseo <yunseo7179@naver.com>
2024-10-25 15:39:18 +0900

    [feat] 찜 기능

---
+++ src/main/java/com/projectw/domain/dibs/repository/DibsRepository.java
@@ -0,0 +1,15 @@
+package com.projectw.domain.dibs.repository;
+
+import com.projectw.domain.dibs.entity.Dibs;
+import com.projectw.domain.store.entity.Store;
+import com.projectw.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DibsRepository extends JpaRepository<Dibs, Long> {
+    List<Dibs> findByUserId(Long userId);
+    Dibs findByUserAndStore(User user, Store store);
+}
