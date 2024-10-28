ac2ae4152d468e87b4109cec56fbc600e35c6bd6
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-28 10:53:43 +0900

    [update] timeunit 최적화

--- src/main/java/com/projectw/common/utils/Scheduler.java
+++ src/main/java/com/projectw/common/utils/Scheduler.java
@@ -1,8 +1,5 @@
 package com.projectw.common.utils;
 
-import com.projectw.common.enums.ResponseCode;
-import com.projectw.common.exceptions.ApiException;
-import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
@@ -17,14 +14,7 @@ public class Scheduler {
 
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-    public <T> void scheduleOnceAfterDelay(long delayTime , String type , Consumer<T> task, T parameter) {
-        TimeUnit timeUnit = switch (type) {
-            case "h" -> TimeUnit.HOURS;
-            case "m" -> TimeUnit.MINUTES;
-            case "s" -> TimeUnit.SECONDS;
-            case "ms" -> TimeUnit.MILLISECONDS;
-            default -> throw new ApiException(HttpStatus.BAD_REQUEST , ResponseCode.INVALID_TIME_UNIT.getMessage());
-        };
+    public <T> void scheduleOnceAfterDelay(long delayTime , TimeUnit timeUnit , Consumer<T> task, T parameter) {
         scheduler.schedule(() -> task.accept(parameter), delayTime, timeUnit);
     }
 
