58e9f334bcf96c0555d4dc46568ac70078230f49
yunseo <yunseo7179@naver.com>
2024-10-29 15:00:40 +0900

    Revert "[feat] 챗봇"
    This reverts commit 382010f06d9858250369d3432ea0e857790f9a3f.

--- src/main/java/com/projectw/domain/chatbot/dto/ChatbotRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.projectw.domain.chatbot.dto;
-
-import lombok.Getter;
-
-@Getter
-public class ChatbotRequestDto {
-    private String inquiryType;
-    private String detail;
-}
--- src/main/java/com/projectw/domain/store/repository/StoreRepository.java
+++ src/main/java/com/projectw/domain/store/repository/StoreRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long>, StoreDslRepository{
@@ -22,8 +21,4 @@ public interface StoreRepository extends JpaRepository<Store, Long>, StoreDslRep
 
     @Query("SELECT s FROM Store s WHERE s.title LIKE %:title% ")
     Page<Store> findAllByTitle(Pageable pageable, String title);
-
-    List<Store> findByDescription(String description); // 특정 카테고리에 속하는 음식점들
-
-    List<Store> findByDescriptionAndAddressStartingWith(String description, String addressStart); // 카테고리와 특정 지역에 맞는 음식점
 }
