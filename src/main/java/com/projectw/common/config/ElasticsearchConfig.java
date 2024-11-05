ea89ababa4be86c0abd107dc7095eece05b04133
Nameless1004 <jjho1029@gmail.com>
2024-11-06 01:39:20 +0900

    [feat] 엘라스틱 서치 properties, config파일 수정

--- src/main/java/com/projectw/common/config/ElasticsearchConfig.java
+++ src/main/java/com/projectw/common/config/ElasticsearchConfig.java
@@ -4,12 +4,10 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,8 +26,8 @@ public class ElasticsearchConfig {
     @Value("${spring.elasticsearch.password}")
     private String elasticsearchPassword;
 
-    @Value("${spring.elasticsearch.encoded.api.key}")
-    private String encodedApiKey;
+//    @Value("${spring.elasticsearch.encoded.api.key}")
+//    private String encodedApiKey;
 
     @Bean
     public ElasticsearchClient elasticsearchClient() {
@@ -40,9 +38,9 @@ public class ElasticsearchConfig {
         try {
             RestClient restClient = RestClient
                     .builder(HttpHost.create(elasticsearchUrl))
-                    .setDefaultHeaders(new Header[]{
-                            new BasicHeader("Authroization", "ApiKey " + encodedApiKey),
-                    })
+//                    .setDefaultHeaders(new Header[]{
+//                            new BasicHeader("Authroization", "ApiKey " + encodedApiKey),
+//                    })
                     .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
                             .setDefaultCredentialsProvider(credentialsProvider)
                     ).build();
