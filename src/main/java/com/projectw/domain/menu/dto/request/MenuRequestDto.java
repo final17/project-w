fe6062be173d0c001ac03811e344b14dd459e38a
giles <pumaclass@naver.com>
2024-11-13 01:52:43 +0900

    feat. 메뉴 이미지 업로드 추가

--- src/main/java/com/projectw/domain/menu/dto/request/MenuRequestDto.java
+++ src/main/java/com/projectw/domain/menu/dto/request/MenuRequestDto.java
@@ -3,6 +3,7 @@ package com.projectw.domain.menu.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -16,7 +17,9 @@ public sealed interface MenuRequestDto permits MenuRequestDto.Create, MenuReques
             @Min(value = 1, message = "가격은 1 이상이어야 합니다.")
             int price,
 
-            List<Long> allergyIds
+            List<Long> allergyIds,
+
+            MultipartFile image
     ) implements MenuRequestDto { }
 
     record Update(
@@ -28,6 +31,8 @@ public sealed interface MenuRequestDto permits MenuRequestDto.Create, MenuReques
             @Min(value = 1, message = "가격은 1 이상이어야 합니다.")
             Integer price,
 
-            List<Long> allergyIds
+            List<Long> allergyIds,
+
+            MultipartFile image
     ) implements MenuRequestDto { }
 }
--- src/main/java/com/projectw/domain/menu/dto/response/MenuResponseDto.java
+++ src/main/java/com/projectw/domain/menu/dto/response/MenuResponseDto.java
@@ -9,12 +9,14 @@ public sealed interface MenuResponseDto permits MenuResponseDto.Detail, MenuResp
             String name,
             int price,
             Set<String> allergies,
-            long viewCount
+            long viewCount,
+            String imageUrl
     ) implements MenuResponseDto { }
 
     record Summary(
             Long id,
             String name,
-            int price
+            int price,
+            String imageUrl
     ) implements MenuResponseDto { }
 }
