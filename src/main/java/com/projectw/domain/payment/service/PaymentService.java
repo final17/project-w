5aac5c4c1926d743ca9c4efa7cc55c39a7e779de
ballqs <ballqs_@naver.com>
2024-11-20 13:27:48 +0900

    [update] payment success 코드 수정

--- src/main/java/com/projectw/domain/payment/service/PaymentService.java
+++ src/main/java/com/projectw/domain/payment/service/PaymentService.java
@@ -137,16 +137,13 @@ public class PaymentService {
         // 예약 가능한지 검증!
         Payment payment = paymentRepository.findByOrderIdAndStatus(susscess.orderId() , Status.PENDING).orElseThrow(() -> new PaymentNotFoundException(ResponseCode.PAYMENT_NOT_FOUND));
 
-        ResponseEntity<String> responseEntity;
-        String responseBody;
-        JsonNode jsonNode;
         ObjectMapper mapper = new ObjectMapper();
 
         RedirectView redirectView = new RedirectView();
 
-        responseEntity = tossPaymentsService.confirm(susscess.paymentKey() , susscess.orderId() , String.valueOf(susscess.amount()));
-        responseBody = responseEntity.getBody();
-        jsonNode = mapper.readTree(responseBody);
+        ResponseEntity<String> responseEntity = tossPaymentsService.confirm(susscess.paymentKey() , susscess.orderId() , String.valueOf(susscess.amount()));
+        String responseBody = responseEntity.getBody();
+        JsonNode jsonNode = mapper.readTree(responseBody);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             confirmPayment(jsonNode);
 
